6e6b24bf71e3778f8598d1d54c8354c3f5711981
Karan-Palan <karanpalan007@gmail.com>
2024-01-04 22:32:14 +0530

    Added events to pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Events from "./components/Events"
-import './App.css'
+import Events from "./pages/Events"
+import './App.css';
+
 
 function App() {
 
   return (
     <>
-    <Events/>
+    <Events></Events>
     </>
     
   )
