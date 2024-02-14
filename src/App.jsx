e190dabecb1cf9e077d824ece7dd360b6d91c323
krishsharma0413 <krishsharma0413@gmail.com>
2024-02-14 19:06:16 +0530

    Add Objective component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Planofaction from './Components/Planofaction.jsx'
 import Footer from './Components/Footer.jsx'
 import Events from './Components/Events.jsx'
 import Navbar from './Components/Navbar.jsx'
-
+import Objective from './Components/Objective.jsx'
 
 function App() {
   return (
     <>
     <Navbar/>
     <Planofaction/>
+    <Objective/>
     <Events/>
     <Footer/>
     </>
