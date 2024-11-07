7183896e03f1b6380e80dd446baec501f30ce4ba
Vranda Garg <gargvranda963@gmail.com>
2024-11-07 17:20:58 +0530

    plan of action text size changed

--- src/Components/Planofaction.jsx
+++ src/Components/Planofaction.jsx
@@ -97,7 +97,7 @@ export default function Planofaction() {
       className="bg-[#000000] py-24 sm:py-32 grid place-content-center"
       id="planofaction "
     >
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-9">
         <div className="mx-auto lg:mx-0 flex justify-center">
           <p className="mt-2 text-3xl font-bold tracking-tight text-[#dadada] sm:text-4xl text-center">
             Plan Of Action
@@ -106,12 +106,15 @@ export default function Planofaction() {
         <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 text-[#dadada] sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16">
           {features.map((feature) => (
             <div className="bg-slate-600/30 p-5 rounded-3xl border-r border-white/50 border-b">
-              <dt className="font-bold text-center text-xl text-[#dadada]">
+              <dt className="font-bold text-center text-base md:text-xl text-[#dadada]">
                 {feature.name}
               </dt>
               <div className="mt-4 space-y-4">
                 {feature.subFeatures.map((subFeature) => (
-                  <div key={subFeature.name} className="relative pl-9">
+                  <div
+                    key={subFeature.name}
+                    className="relative pl-9  text-sm md:text-base"
+                  >
                     <dt className="inline font-semibold text-[#dadada]">
                       <subFeature.icon
                         className="absolute left-1 top-1 h-5 w-5 text-indigo-500"
