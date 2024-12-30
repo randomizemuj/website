4f6df239e8cb2d157e6b858d80a62d7f9cb1f1ba
Rishi <151076847+Risheeshh@users.noreply.github.com>
2024-12-31 00:37:03 +0530

    Removed bug in projects page mobile view by changing footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -40,7 +40,7 @@ const navigation = {
 export default function Footer() {
   return (
     <footer className="bg-[#030712]">
-      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+      <div className="mx-auto max-w-[100vw] overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
         <nav
           className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
           aria-label="Footer"
